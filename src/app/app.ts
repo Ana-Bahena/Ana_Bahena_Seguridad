e38511aa57c8e2a0f7a3a716dd77a7308482d439
Ana Isabel Bahena Sanchez <anabahena263@gmail.com>
2026-02-20 16:20:27 -0600

    Commit - Botón PrimeNG

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ButtonModule],  
+  standalone: true,
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Ana_Isabel_Bahena_Sanchez-angular');
+}
